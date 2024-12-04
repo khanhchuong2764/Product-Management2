8d89e57c85946224d0a9ab9a2e03a9e731f724e0
khanhchuong2764 <khanhchuong2764@gmail.com>
2024-12-04 14:32:31 +0700

    Lay data in gd

--- controller/admin/product.controllers.js
+++ controller/admin/product.controllers.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Product = require("../../model/product.model");
+
+
+module.exports.index = async(req, res) => {
+    const product = await Product.find({
+        deleted:false
+    });
+    console.log(product);
     res.render("admin/pages/products/index",{
-        titlePage:"Danh Sách Sản Phẩm"
+        titlePage:"Danh Sách Sản Phẩm",
+        product:product
     })  
 }
