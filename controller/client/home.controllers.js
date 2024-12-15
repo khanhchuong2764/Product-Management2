cd4a77472674dd4ed872ec44524115d9b141b0da
khanhchuong2764 <khanhchuong2764@gmail.com>
2024-12-15 18:33:28 +0700

    Hiển thị sản phẩm mới

--- controller/client/home.controllers.js
+++ controller/client/home.controllers.js
@@ -11,8 +11,16 @@ module.exports.index = async (req, res) => {
             featured : "1"
         }).limit(5);
     const Newproduct = ProductHelper.GetPriceNew(ProductFeature);
+
+    const productsNew = await Product.find({
+        deleted :false ,
+        status :"active"
+    }).sort({posittion:"desc"}).limit(5);
+
+    const ProductNewNew = ProductHelper.GetPriceNew(productsNew);
     res.render("client/pages/home/index",{
         titlePage:"Trang Chu",
-        ProductFeature:Newproduct
+        ProductFeature:Newproduct,
+        ProductNew :ProductNewNew 
     })
 }
